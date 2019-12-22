1b041caa361b82b7fd56e826a120ad3b678cbbed
Reuben Ogbonna <reubenogbonna@gmail.com>
2019-12-22 16:44:17 -0500

    day two updates

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
 const port = 3000;
 const TodoList = require('./src/TodoList');
 
 app.set('view engine', 'ejs');
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(expressLayouts);
+app.use(express.static('public'));
 
 const todoList = new TodoList('Your Task List')
 
@@ -24,6 +28,17 @@ app.post('/delete_task', (req, res) => {
   let task_id = Number(Object.keys(req.body)[0]);
   todoList.deleteTask(task_id);
   res.render('index', { todoList })
+});
+
+app.get('/test', (req, res) => {
+  res.render('just-checking', { todoList })
+});
+
+app.post('/update_task', (req, res) => {
+  let taskIdStr = Object.keys(req.body)[0];
+  const task = todoList.tasks.find((task) => task.task_id === Number(taskIdStr));
+  task.updateTask(req.body[taskIdStr]);
+  res.render('index', { todoList });
 });
 
 app.listen(port, () => console.log(`Now listening on port ${port}...`));
